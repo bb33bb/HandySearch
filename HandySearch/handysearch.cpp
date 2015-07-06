a2b51635eef4f3546594db00b5ddccac99959219
RyanWangGit <wangyx1995@hotmail.com>
2015-07-06 09:16:27 +0800

    Initial Commit.

---
+++ HandySearch/handysearch.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "handysearch.h"
+
+HandySearch::HandySearch(QWidget *parent)
+	: QMainWindow(parent)
+{
+	ui.setupUi(this);
+}
+
+HandySearch::~HandySearch()
+{
+
+}
---
+++ HandySearch/handysearch.h
@@ -0,0 +1,19 @@
+#ifndef HANDYSEARCH_H
+#define HANDYSEARCH_H
+
+#include <QtWidgets/QMainWindow>
+#include "ui_handysearch.h"
+
+class HandySearch : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	HandySearch(QWidget *parent = 0);
+	~HandySearch();
+
+private:
+	Ui::HandySearchClass ui;
+};
+
+#endif // HANDYSEARCH_H
