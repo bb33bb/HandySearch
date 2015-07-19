d56a1392ed9e192eb4dfc0fb96b4de6d41289e90
RyanWangGit <wangyx1995@hotmail.com>
2015-07-19 10:59:34 +0800

    Bug fixed where function statement shouldn't include "static"

---
+++ HandySearch/HashMap.h
@@ -0,0 +1,62 @@
+#pragma once
+template < typename K, typename V >
+class HashMap
+{
+public:
+	static int hashCode(const void * key, int len);
+};
+
+/* We use Murmur 2.0 hash function here */
+/* and use 0xEE6B27EB as the seed */
+template < typename K, typename V >
+int HashMap<K, V>::hashCode(const void * key, int len)
+{
+	// 'm' and 'r' are mixing constants generated offline.
+	// They're not really 'magic', they just happen to work well.
+
+	const unsigned int seed = 0xEE6B27EB;
+	const unsigned int m = 0x5bd1e995;
+	const int r = 24;
+
+	// Initialize the hash to a 'random' value
+
+	unsigned int h = seed ^ len;
+
+	// Mix 4 bytes at a time into the hash
+
+	const unsigned char * data = (const unsigned char *)key;
+
+	while (len >= 4)
+	{
+		unsigned int k = *(unsigned int *)data;
+
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+
+		h *= m;
+		h ^= k;
+
+		data += 4;
+		len -= 4;
+	}
+
+	// Handle the last few bytes of the input array
+
+	switch (len)
+	{
+	case 3: h ^= data[2] << 16;
+	case 2: h ^= data[1] << 8;
+	case 1: h ^= data[0];
+		h *= m;
+	};
+
+	// Do a few final mixes of the hash to ensure the last few
+	// bytes are well-incorporated.
+
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+
+	return h;
+}
