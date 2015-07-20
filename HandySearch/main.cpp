5a61c7b870576dc279f5c29dfe4079b965e69d15
RyanWangGit <wangyx1995@hotmail.com>
2015-07-20 18:48:41 +0800

    Put the tests in the Main GUI (Core) thread , instead of putting them in int main().

--- HandySearch/main.cpp
+++ HandySearch/main.cpp
@@ -9,13 +9,6 @@ int main(int argc, char *argv[])
 	w.setWindowIconText("HandySearch");
 	w.setWindowFlags(Qt::FramelessWindowHint);
 	w.setAttribute(Qt::WA_TranslucentBackground);
-	
-	//Class tests
-	Html html = Html("D:\\Converted\\17.html");
-	
-	List<Html> list;
-	//End of class tests
-	
 	w.show();
 	return a.exec();
 }
