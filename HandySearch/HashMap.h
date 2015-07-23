f80a7e4991eddde752e90ccd0d0394df390f5001
RyanWangGit <wangyx1995@hotmail.com>
2015-07-24 00:17:39 +0800

    Add HashMap data structure.

--- HandySearch/HashMap.h
+++ HandySearch/HashMap.h
@@ -2,14 +2,40 @@
 template < typename K, typename V >
 class HashMap
 {
+#define INDEX_SIZE 1048576 //2^20
+private:
+	List<V> *index;
+	float loadFactor;
 public:
-	static int hashCode(const void * key, int len, const unsigned int seed = 0xEE6B27EB);
+	HashMap();
+	~HashMap();
+	bool put(const K &key,const V &value);
+	V& get(const K &key);
+	bool resize(int newSize);
+
+	/* Static methods */
+	static unsigned int hashCode(const void * key, int len, const unsigned int seed = 0xEE6B27EB);
 };
+
+template < typename K, typename V >
+HashMap<K, V>::HashMap()
+{
+	this->index = new List<V>[INDEX_SIZE];
+	//TODO
+}
+
+template < typename K, typename V >
+bool HashMap<K, V>::put(const K &key, const V &value)
+{
+	unsigned int index = HashMap::hashCode((void *)&key,)
+		//TODO
+}
+
 
 /* We use Murmur 2.0 hash function here */
 /* and use 0xEE6B27EB as the seed */
 template < typename K, typename V >
-int HashMap<K, V>::hashCode(const void * key, int len, const unsigned int seed = 0xEE6B27EB)
+unsigned int HashMap<K, V>::hashCode(const void * key, int len, const unsigned int seed = 0xEE6B27EB)
 {
 	// 'm' and 'r' are mixing constants generated offline.
 	// They're not really 'magic', they just happen to work well.
