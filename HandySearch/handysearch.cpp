7d5cd0dd61a3bab6d0723d47696d0757ce686ec8
RyanWangGit <wangyx1995@hotmail.com>
2015-07-24 00:16:08 +0800

    Add some UI test and correct the thread usage.

--- HandySearch/handysearch.cpp
+++ HandySearch/handysearch.cpp
@@ -3,22 +3,50 @@
 
 using namespace std;
 
+unsigned int LoadHtml::threadNum = 0;
+
 HandySearch::HandySearch(QWidget *parent)
 	: QMainWindow(parent)
 {
 	qRegisterMetaType<Html>("Html");
 	ui.setupUi(this);
+
+	//Start the application clock
+	clock.start();
+
+	//The default html library path
+	QString htmlFolder = "C:/Html Library";
+	QDir dir(htmlFolder);
+	this->ui.progressBar->setRange(0, dir.entryList().size());
+
+	//The default dictionary library path
+	QString dictFolder = "C:/Dictionary Library";
+
 	//Initialization of application
-	//Test of classes
-	Load *initialLoadThread = new Load(this->htmlList);
-	connect(initialLoadThread, &Load::finished, this, &HandySearch::loadFinished);
-	connect(initialLoadThread, &Load::finished, initialLoadThread, &Load::deleteLater);
+	QThread *initialLoadThread = new QThread();
+	Load *initialLoad = new Load(this->htmlList, htmlFolder, dictFolder);
+	initialLoad->moveToThread(initialLoadThread);
+	connect(initialLoadThread, &QThread::started, initialLoad, &Load::run);
+	connect(initialLoad, &Load::finished, this, &HandySearch::loadFinished);
+	connect(initialLoad, &Load::finished, initialLoad, &QObject::deleteLater);
+	connect(initialLoad, &Load::finished, initialLoadThread, &QThread::quit);
+	connect(initialLoadThread, &QThread::finished, initialLoadThread, &QObject::deleteLater);
+
+	//Connect the thread signals to the UI slots
+	connect(initialLoad, &Load::loadedHtml, this, &HandySearch::processHtml);
 	initialLoadThread->start();
+
 }
 
 HandySearch::~HandySearch()
 {
 	
+}
+
+
+void HandySearch::processHtml(unsigned int threadID, Html html, QString path)
+{
+	this->ui.progressBar->setValue(this->ui.progressBar->value() + 1);
 }
 
 void HandySearch::paintEvent(QPaintEvent *event)
@@ -60,6 +88,7 @@ void HandySearch::paintEvent(QPaintEvent *event)
 void HandySearch::loadFinished()
 {
 	qDebug() << "Load finished " << this->htmlList.size();
+	qDebug() << "Time elapsed: " << clock.elapsed() << "with List size:" << this->htmlList.size();
 }
 //This is a test method
 void HandySearch::onCloseButtonClick()
